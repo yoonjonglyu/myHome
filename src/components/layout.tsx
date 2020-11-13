93d619c5a1253a1ad9198f25c7c08c0b75ca46ad
Isa(yunjongryu) <yunjonglyu@gmail.com>
2020-11-13 22:32:24 +0900

    feat: LAYOUT CONTAINER STYLE

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -12,7 +12,7 @@ const Layout : React.FC<LayoutProps> = ({ children }) => {
     return (
         <div className="wrap">
             <Header />
-            <Container maxWidth="sm">
+            <Container maxWidth="sm" style={{ borderLeft: '1px solid black', borderRight: '1px solid black', paddingTop : '12px', minHeight : '90vh'}}>
                {children}
             </Container>
             <Footer />
