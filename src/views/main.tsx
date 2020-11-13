98dd0b99aa36c7c578e84c23efd8c12a6cb78be2
Isa(yunjongryu) <yunjonglyu@gmail.com>
2020-11-13 23:36:03 +0900

    feat: MAIN VIEWS - DESIGN

--- src/views/main.tsx
+++ src/views/main.tsx
@@ -9,7 +9,21 @@ interface MainProps {
 const Main : React.FC<MainProps> = () => {
     return(
         <Layout>
-            <h1>메인</h1>
+            <React.Fragment>
+            <section className="main-headline">
+                <h1>메인</h1>
+                <p>타이핑 느낌의 인사말 넣고</p>
+            </section>
+            <section className="main-about">
+                <p>자기소개 </p>
+            </section>
+            <section className="main-skill">
+                <p>기술소개</p>
+            </section>
+            <section className="main-portfolio">
+                <p>포트폴리오 간략히 소개</p>
+            </section>
+            </React.Fragment>
         </Layout>
     );
 }
