2d30b82d81cd1164fe7756c2638e7456ebcfb87a
Isa(yunjongryu) <yunjonglyu@gmail.com>
2020-11-13 19:09:16 +0900

    feat: FOOTER COMPONENTS

---
+++ src/components/footer.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Paper from '@material-ui/core/Paper';
+
+interface FooterProps {
+}
+
+const Footer: React.FC<FooterProps> = () => {
+    return (
+        <footer >
+            <Paper>
+                <small>
+                    Copyright© ISA(yoonjonglyu)
+                </small>
+            </Paper>
+
+
+        </footer>
+    );
+};
+
+
+export default Footer;
