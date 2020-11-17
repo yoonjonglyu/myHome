d531689a9d113d7778a989911eeea6fe181f6663
Isa(yunjongryu) <yunjonglyu@gmail.com>
2020-11-17 12:47:08 +0900

    refactor: REDUX ACTIONS STRUCTURE

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,10 +1,5 @@
-import * as types from './actionTypes';
-
-export const TAPINDEX = (index : number) => {
-    return {
-        type : types.TAP_INDEX,
-        payload : index
-    };
-};
-
-export type TapActions = ReturnType<typeof TAPINDEX>;
+/**
+ * @descriptions redux action & type
+ */
+export {TAPINDEX, TapActions} from './tapIndex';
+export {TAGLIST, TagActions} from './tagList';
