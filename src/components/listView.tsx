19c71e71be5260db7fd06c36c53daf202f6d3bd9
Isa(yunjongryu) <yunjonglyu@gmail.com>
2020-11-17 13:28:14 +0900

    feat: LIST VIEW COMPONENTS - STORE

--- src/components/listView.tsx
+++ src/components/listView.tsx
@@ -4,10 +4,14 @@ import { Grid } from '@material-ui/core';
 
 import ListItem from './listItem';
 
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
 interface ListViewProps {
+    data : Array<any>
 }
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
     root: {
         flexGrow: 1,
     }
@@ -15,14 +19,19 @@ const useStyles = makeStyles((theme) => ({
 
 const ListView: React.FC<ListViewProps> = (props) => {
     const classes = useStyles();
+    const {
+        data
+    } = props;
 
-    const data = [
+    const tempData = [// 해당 데이터를 어떻게 만드냐에 따라 뷰가 달라진다. 뷰모델 부분에서 가공할 필요가 있다.
         [{ name: 'test1' }, { name: 'test1232323' }, { name: 'test1aasdsada' }],
         [{ name: 'test1' }, { name: 'test1232323' }, { name: 'test1aasdsada' }],
         [{ name: 'test1' }, { name: 'test1232323' }, { name: 'test1aasdsada' }]
     ];
+    
+    const tagList = data.length > 0 ? data : tempData; 
 
-    const ListRow = data.map((items: Array<any>, key : number) => {
+    const ListRow = tagList.map((items: Array<any>, key : number) => {
         return (
             <Grid container item xs={12} spacing={1} key={key}>
                 <ListItem children={items} />
@@ -37,4 +46,16 @@ const ListView: React.FC<ListViewProps> = (props) => {
     );
 };
 
-export default ListView;
+const mapStateToProps = ({ TagList } : React.ComponentState) => {
+    return {
+        data : TagList.tagList
+    };
+};
+
+const mapDispatchToProps = (dispatch : React.Dispatch<Function>) => {
+    return {
+        
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ListView);
