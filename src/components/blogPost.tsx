12a1ec8d9526f9f45b05e58baf5e5ba45ed70c7d
Isa(yunjongryu) <yunjonglyu@gmail.com>
2020-11-19 23:16:56 +0900

    feat: BLOG POST COMPONENTS - CONTENTS BASIC DESIGN

--- src/components/blogPost.tsx
+++ src/components/blogPost.tsx
@@ -8,7 +8,7 @@ const BlogPostProps: React.FC<BlogPostProps> = () => {
     return (
         <React.Fragment>
             <header className="post-header">
-                <h1 className="post-title">블로그 글</h1>
+                <h2 className="post-title">블로그 글</h2>
                 <p className="post-meta">
                 <time dateTime="2020-11-18T11:15:00+09:00">js date format</time>
                 <span className="author">
@@ -17,9 +17,19 @@ const BlogPostProps: React.FC<BlogPostProps> = () => {
                 </p>
             </header>
             <div className="post-contents">
+                <h3>소제목은 h3로</h3>
                 <p>
-                    글 어떻게 구성하고 스타일링 할까? 글에는 당연히 텍스트는 들어갈거고,
-                    이미지나 해드라인, 링크등은 어떻게 처리할까?
+                    글 은 기본적으로  p태그 안에 쓰고 단락마다 p태그를 열고 닫는다.
+                </p>
+                <img src="#/" alt="이미지는 img src로 단락 밖에" />
+                <p>
+                    이렇게 말이다. 해드라인도 p태그 밖에 위치한다.<br />
+                    코드는 p태그 안에 span 태그로 감싸서 처리한다.
+                    <span className="post-code">
+                        code : js
+                        const foo = bar;
+                        console.log(foo);
+                    </span>
                 </p>
             </div>
             <div className="post-comments">
