3c071c9ed032618534f1db3fe0b0639005e20ac1
Isa(yunjongryu) <yunjonglyu@gmail.com>
2020-12-01 22:37:48 +0900

    feat: BLOG POST COMPONENTS - DISQUS COMMENTS

--- src/components/blogPost.tsx
+++ src/components/blogPost.tsx
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 interface BlogPostProps {
 
 }
 
 const BlogPostProps: React.FC<BlogPostProps> = () => {
+    const Disqus = () => {
+        /**
+        *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+        *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables    */
+        /*
+        var disqus_config = function () {
+        this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+        this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+        };
+        */
+        (function () { // DON'T EDIT BELOW THIS LINE
+            const d = document, s = d.createElement('script');
+            s.src = 'https://https-yoonjonglyu-github-io-myhome.disqus.com/embed.js';
+            s.setAttribute('data-timestamp', `${new Date()}`);
+            (d.head || d.body).appendChild(s);
+        })();
+    }
+
+    useEffect(() => {
+        Disqus();
+    }, []);
+
     return (
         <React.Fragment>
             <header className="post-header">
                 <h2 className="post-title">블로그 글</h2>
                 <p className="post-meta">
-                <time dateTime="2020-11-18T11:15:00+09:00">js date format</time>
-                <span className="author">
-                    (공백엑 스타일링)  ISA
+                    <time dateTime="2020-11-18T11:15:00+09:00">js date format</time>
+                    <span className="author">
+                        (공백엑 스타일링)  ISA
                 </span>
                 </p>
             </header>
@@ -26,10 +48,10 @@ const BlogPostProps: React.FC<BlogPostProps> = () => {
                     이렇게 말이다. 해드라인도 p태그 밖에 위치한다.<br />
 
                 </p>
-                <pre className="post-code" style={{border : "1px solid red"}}>
-                        code : js
-                        const foo = bar;
-                        console.log(foo);
+                <pre className="post-code" style={{ border: "1px solid red" }}>
+                    code : js
+                    const foo = bar;
+                    console.log(foo);
                 </pre>
                 <p>
                     코드는 pre로 감싸서 p태그 밖의 스타일링은 class를 통해서가 아니라 태그 자체로 스타일링 해야할거 같다.
@@ -39,8 +61,9 @@ const BlogPostProps: React.FC<BlogPostProps> = () => {
                 태그 목록들 그리드로 넣으면 되지 않을까 생각중
             </div>
             <div className="post-comments">
-                댓글 들갈 부분
+                <div id="disqus_thread"></div>
             </div>
+            <script id="dsq-count-scr" src="//https-yoonjonglyu-github-io-myhome.disqus.com/count.js" async></script>
         </React.Fragment>
     );
 };
