4c1bf1b42ced9c61749a8a0b1ed06e281913670d
Isa(yunjongryu) <yunjonglyu@gmail.com>
2020-12-01 17:40:06 +0900

    feat: POST LIST COMPONENTS - PROPS DATA & STYLE

--- src/components/postList.tsx
+++ src/components/postList.tsx
@@ -2,8 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
+interface PostProps {
+    postIdx : number
+    postDate : string
+    postAuthor : string 
+    postTitle : string 
+    postDescription : string
+}
+
 interface PostListProps {
-    postList : Array<any>
+    postList : Array<PostProps> 
 }
 
 const ContentsItem = styled.div`
@@ -13,9 +21,17 @@ const ContentsItem = styled.div`
 const PostLink = styled(Link)`
     text-decoration : none;
     color : unset;
-`
+`;
+const MetaText = styled.span`
+    font-size : 0.9rem;
+    color : gray;
+`;
 const Headline = styled.h2`
     font-size : 2rem;
+`;
+const SubText = styled.p`
+    font-size : 1rem;
+    color : #0f4c75;
 `;
 
 const PostList: React.FC<PostListProps> = (props) => {
@@ -26,12 +42,12 @@ const PostList: React.FC<PostListProps> = (props) => {
     const List = postList.map((post, key) => {
         return (
             <ContentsItem key={key}>
-                <PostLink to={`?${key}`}>
-                    <span><time dateTime="2020-12-01">2020.12.01</time> 류윤종</span>
-                    <Headline>포트폴리오 title이랴 이랴 이랴 ! 이랴!! 이랴랴랼! 이랴!</Headline>
-                    <p>
-                    낙원을 피어나는 되는 긴지라 피다. 바이며, 피가 이것이야말로 위하여 쓸쓸한 이것이다. 대고, 인간의 능히 있으랴? 두손을 이성은 용감하고 있는 끓는 보내는 사막이다. 찾아다녀도, 아름답고 찬미를 청춘의 이것이다. 동산에는 긴지라 대한 이상은 쓸쓸하랴? 곧 구하지 인간이 청춘의 얼마나 반짝이는 그들은 봄바람이다. 아니한 예가 같으며, 이것이다. 커다란 청춘 그림자는 이상 같이, 그들의 것이다.
-                    </p>
+                <PostLink to={`?post=${post.postIdx}`}>
+                    <MetaText className="post-meta"><time dateTime={post.postDate}>{post.postDate}</time> {post.postAuthor}</MetaText>
+                    <Headline className="post-title">{post.postTitle}</Headline>
+                    <SubText className="post-description">
+                        {post.postDescription}
+                    </SubText>
                 </PostLink>
             </ContentsItem>
         );
