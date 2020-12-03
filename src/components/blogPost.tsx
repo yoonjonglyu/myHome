9eec6b0e6fc3a83b8ebf8dad227285e583817cf3
Isa(yunjongryu) <yunjonglyu@gmail.com>
2020-12-04 03:39:38 +0900

    feat: BLOG POST COMPONENTS - ADD GLOBAL STYLED

--- src/components/blogPost.tsx
+++ src/components/blogPost.tsx
@@ -1,14 +1,54 @@
 import React, { useEffect } from 'react';
+import styled, { createGlobalStyle } from 'styled-components';
 
-import TagList from '../components/TagList';
+import TagList from './tagList';
+
+import test from '../assets/images/sam-type.jpg';
 
 interface BlogPostProps {
-
+    PostData : Object
 }
+
+const BlogPostStyle = createGlobalStyle`
+    h2 {
+        font-size : 2rem;
+        text-align : center;
+    }
+    h3 {
+        font-size : 1.5rem;
+        color : #f08a5d;
+    }
+    p {
+        font-size : 1.1rem;
+        line-height : 1.8rem;
+        letter-spacing : 0;
+    }
+    img {
+        width : 100%;
+        margin : 8px 0; 
+    }
+    pre {
+        margin: 8px 0;
+        padding: 20px 16px;
+        font-size : 1.1rem;
+        line-height : 1.5rem;
+        color : #eeeeee;
+        background-color : #222831;
+    }
+    pre span {
+        color : #00adb5;
+    }
+    pre code {
+        color : #b83b5e;
+    }
+
+`;
+const Fc = styled.code`
+    color : #f08a5d;
+`;
 
 const BlogPostProps: React.FC<BlogPostProps> = () => {
     const Disqus = (disqus_title: string, disqus_url: string) => {
-        console.log(disqus_url)
         /**
         *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
         *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables    */
@@ -49,6 +89,7 @@ const BlogPostProps: React.FC<BlogPostProps> = () => {
 
     return (
         <React.Fragment>
+            <BlogPostStyle />
             <header className="post-header">
                 <h2 className="post-title">블로그 글</h2>
                 <p className="post-meta">
@@ -61,17 +102,18 @@ const BlogPostProps: React.FC<BlogPostProps> = () => {
             <div className="post-contents">
                 <h3>소제목은 h3로</h3>
                 <p>
-                    글 은 기본적으로  p태그 안에 쓰고 단락마다 p태그를 열고 닫는다.
+                    글은 기본적으로  p태그 안에 쓰고 단락마다 p태그를 열고 닫는다.<br />
+                    여러 라인으로 글을 쓰고 글 간격이나 기타등등<br />
+                    을 조정
                 </p>
-                <img src="#/" alt="이미지는 img src로 단락 밖에" />
+                <img src={test} alt="이미지는 img src로 단락 밖에" />
                 <p>
-                    이렇게 말이다. 해드라인도 p태그 밖에 위치한다.<br />
-
+                    이렇게 말이다. 해드라인도 p태그 밖에 위치한다.
                 </p>
-                <pre className="post-code" style={{ border: "1px solid red" }}>
-                    code : js
-                    const foo = bar;
-                    console.log(foo);
+                <pre className="post-code">
+                    <span>code : js</span><br/>
+                    <code>const</code> foo = bar;<br />
+                    <Fc>console.log(foo);</Fc>
                 </pre>
                 <p>
                     코드는 pre로 감싸서 p태그 밖의 스타일링은 class를 통해서가 아니라 태그 자체로 스타일링 해야할거 같다.
