df58b74d4e2b340ef1fa80ba518772e51f6295b4
Isa(yunjongryu) <yunjonglyu@gmail.com>
2020-12-04 22:01:18 +0900

    feat: INTRO COMPONENTS - MARK UP & ANIMATION & STYLE

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -11,26 +11,42 @@ const Cursor = keyframes`
     50%{border-right: 1px solid #000} 
     100%{border-right: 1px solid #fff} 
 `;
+const StarLight = keyframes`
+    0%{opacity : 0.9} 
+    50%{opacity : 0.8} 
+    100%{opacity : 1} 
+`;
 const IntroBox = styled.div`
     width : 100%;
-    min-height : 400px;
+    min-height : 380px;
     text-align : center;
     color : white;
     background-image : url('${BackGroundIMG}');
     background-size : cover;
 `;
-const Headline = styled.h1`
+const Headline = styled.h1<{isEnd : boolean}>`
     display : inline-block;
+    margin : 90px 0 20px 0;
+    font-size : ${props => props.isEnd ? "2rem" : "2.5rem"};
+    line-height : 2rem;
+    color : #eaeaea;
     animation : ${Cursor} 0.5s step-end infinite;
     animation : ${props => props.isEnd ? '' : "none"}
 `;
-const AboutHead = styled.span`
+const AboutHead = styled.span<{isEnd : boolean}>`
     display : inline-block;
+    margin : 20px 8px;
+    font-size : 1.3rem;
+    line-height : 1.6rem;
+    color : #08d9d6;
     animation : ${Cursor} 0.5s step-end infinite;
-    animation : ${props => props.isEnd ? '' : "none"}
+    animation : ${props => props.isEnd ? '' : StarLight} 0.5s step-end infinite;
 `;
 const IntroText = styled.span`
     display : inline-block;
+    font-size : 1.2rem;
+    line-height : 1.5rem;
+    color : #ff2e63;
     animation : ${Cursor} 0.5s step-end infinite;
 `;
 
@@ -38,9 +54,9 @@ const Intro: React.FC<IntroProps> = () => {
     const helloText = "Hello, World!";
     const aboutText = "소프트웨어 장인을 목표로 노력하는 개발자 류윤종입니다.";
     const subText = [
-        "JavaScript를(TypeScript) 이용한 풀스택 개발을 합니다.",
+        "JavaScript(TypeScript)를 이용한 풀스택 개발을 지향합니다.",
         "클린 코드, 누구나 이해하기 쉬운 코드 견고한 구조를 지향합니다.",
-        "늘 지식의 영역을 넘어서 지혜의 영역을 고민합니다."
+        "늘 표면을 넘어 그 너머의 원리를 고민합니다."
     ];
     const [headline, setHeadline] = useState("");
     const [about, setAbout] = useState("");
@@ -48,30 +64,35 @@ const Intro: React.FC<IntroProps> = () => {
     const [introIndex, setIntroIndex] = useState(0);
     const [headAvail, setHeadAvail] = useState(true);
     const [aboutAvail, setAboutAvail] = useState(true);
+    const [time, setTime] = useState(100);
 
     const typing = (dom: React.Dispatch<React.SetStateAction<string>>, text: string) => {
         dom(text);
     }
 
     const typeIntro = () => {
-        if (headline.length < helloText.length) {
+        if (headline.length < helloText.length) { // hello world typing
             const state = helloText.split('');
             typing(setHeadline, `${headline}${state[headline.length]}`);
-        } else if (about.length < aboutText.length) {
+        } else if (about.length < aboutText.length) { // about typing
             const state = aboutText.split('');
             typing(setAbout, `${about}${state[about.length]}`);
 
-            if (headAvail === true) {
+            if (headAvail === true) { 
                 setHeadAvail(false);
             }
-        } else if (intro.length < subText[introIndex].length) {
+        } else if (intro.length < subText[introIndex].length) { // intro typing
+            if (intro.length === (subText[introIndex].length - 1)) { // 문장 완성 후 잠시 멈춤
+                setTime(800);
+            }
             const state = subText[introIndex].split('');
             typing(setIntro, `${intro}${state[intro.length]}`);
 
             if (aboutAvail === true) {
                 setAboutAvail(false);
+                setTime(150);
             }
-        } else {
+        } else { // intro 초기화
             let state = 0;
             if (introIndex + 1 < subText.length) {
                 state = 1 + introIndex;
@@ -80,12 +101,13 @@ const Intro: React.FC<IntroProps> = () => {
                 state = 0;
                 setIntroIndex(0);
             }
+            setTime(150);
             typing(setIntro, subText[state][0]);
         }
     };
 
     useEffect(() => {
-        setTimeout(typeIntro, 50);
+        setTimeout(typeIntro, time);
     }, [headline, about, intro]);
 
     return (
