b8c6479f889bd285baa181091fe042d90f085b71
Isa(yunjongryu) <yunjonglyu@gmail.com>
2020-12-04 19:21:21 +0900

    chore: TYPE SCRIPT - ADD PNG TYPE

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -2,6 +2,10 @@ declare module '*.jpg' {
     const content: string;
     export default content;
 }
+declare module '*.png' {
+  const content: string;
+  export default content;
+}
 declare module '*.pdf' {
   const content : string;
   export default content;
