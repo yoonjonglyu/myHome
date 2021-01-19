08d2913418dbe42fa1c405085d04c685c87880d9
Isa(yunjongryu) <yunjonglyu@gmail.com>
2021-01-19 23:32:09 +0900

    fix: API - GET ALL TAGS GET DATA NOT THEN

--- src/api/blogApi.ts
+++ src/api/blogApi.ts
@@ -1,17 +1,17 @@
 import axios from 'axios';
 
 class BlogApi {
-    _REST_API_URL : string
+    _REST_API_URL : string;
 
     constructor() {
         this._REST_API_URL = "https://isa-myblog.herokuapp.com/post/";
-    }
+    };
 
     getAllTags =  () => {
-        axios.get(this._REST_API_URL + "tags", ).then((res) => {
-            console.log(res);
-        })
-    }
+        return axios.get(
+            this._REST_API_URL + "tags"
+        );
+    };
 }
 
 export default new BlogApi();
