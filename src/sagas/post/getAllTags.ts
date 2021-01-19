bb8a52a2c9be23c0f1b6b7a8a677d7e0603e329d
Isa(yunjongryu) <yunjonglyu@gmail.com>
2021-01-19 23:32:43 +0900

    feat: SAGA - GET ALL TAGS DISPATCH TAGLIST

--- src/sagas/post/getAllTags.ts
+++ src/sagas/post/getAllTags.ts
@@ -1,7 +1,19 @@
+import { put, call} from 'redux-saga/effects';
 import api from '../../api/blogApi';
+import * as actions from '../../actions';
 
 function* getAllTags () {
-    api.getAllTags()
+    console.log("태그목록을 성공적으로 불러왔습니다.");
+    try {
+        const { data } = yield call(api.getAllTags);
+        const state : any = {};
+        state.essay = [data.essay];
+        state.tech = [data.tech];
+        state.portfolio = [data.portfolio];
+        yield put(actions.TAGLIST(state));
+    } catch(error) {
+
+    }
 }
 
 export default getAllTags;
