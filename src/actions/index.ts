72983a4fb7d17d1e922ba7944fe2eb9892789c04
Isa(yunjongryu) <yunjonglyu@gmail.com>
2021-01-26 22:23:02 +0900

    feat: REDUX ACTIONS - INDEX REQUIRE & IMPORT

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -4,3 +4,4 @@
 export {TAPINDEX, TapActions} from './tapIndex';
 export {TAGLIST, TagActions} from './tagList';
 export {POSTLIST, PostActions} from './postList';
+export {POSTCONTENTS, BlogPostActions} from './postContents';
