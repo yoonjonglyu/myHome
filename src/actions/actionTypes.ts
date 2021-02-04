0f2ec20834b0c94bf4ec2745c07dda24420fd86b
Isa(yunjongryu) <yunjonglyu@gmail.com>
2021-02-04 21:56:09 +0900

    feat: REDUX ACTIONS TYPE - LOAD POST CONTENTS

--- src/actions/actionTypes.ts
+++ src/actions/actionTypes.ts
@@ -2,4 +2,5 @@ export const TAP_INDEX : string = "tapIndex";
 export const TAG_LIST : string = "tagList";
 export const LOAD_POST_LIST : string = "loadPostList";
 export const POST_LIST : string = "postList";
+export const LOAD_POST_CONTENTS : string = "loadPostCotents";
 export const POST_CONTENTS : string = "postContents";
