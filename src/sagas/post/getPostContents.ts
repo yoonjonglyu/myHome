b078a7ebce744e7c9693103aa67d320cb235f91b
Isa(yunjongryu) <yunjonglyu@gmail.com>
2021-02-04 22:39:38 +0900

    fix: SAGAS POST - GET POST CONTENTS

--- src/sagas/post/getPostContents.ts
+++ src/sagas/post/getPostContents.ts
@@ -2,11 +2,10 @@ import { put, call} from 'redux-saga/effects';
 import api from '../../api/blogApi';
 import * as actions from '../../actions';
 
-function* getPostContents (type : string, idx : number) {
+function* getPostContents (action : {type : string, payload : {type : string, idx : string}}) {
     try {
-        const { data } = yield call(api.getPostContents, type, idx);
-        const responsePost = data[type];
-        
+        const { data } = yield call(api.getPostContents, action.payload.type, action.payload.idx);
+        const responsePost = data[action.payload.type];
         yield put(actions.POSTCONTENTS(responsePost));
     } catch(error) {
 
