4bf2230e8dc579b29183cb32ea3b8968cf9f6709
Isa(yunjongryu) <yunjonglyu@gmail.com>
2021-02-04 22:39:02 +0900

    feat: REDUX ACTIONS - ROOT ACTION

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -5,4 +5,5 @@ export {TAPINDEX, TapActions} from './tapIndex';
 export {TAGLIST, TagActions} from './tagList';
 export {LOADPOSTLIST, LoadPostActions} from './loadPostList';
 export {POSTLIST, PostActions} from './postList';
+export {LOADPOSTCONTENTS, LoadBlogPostActions} from './loadPostContents';
 export {POSTCONTENTS, BlogPostActions} from './postContents';
