58e1275872ff7f76d4429e81e1e25550fc221d4d
Isa(yunjongryu) <yunjonglyu@gmail.com>
2021-02-14 17:58:43 +0900

    feat: SAGAS ROOT SAGAS - TAKE GET TAG POST LIST

--- src/sagas/index.ts
+++ src/sagas/index.ts
@@ -4,13 +4,14 @@ import * as actionType  from '../actions/actionTypes';
 import getAllTags from './post/getAllTags';
 import getPostList from './post/getPostList';
 import getPostContents from './post/getPostContents';
-
+import getTagPostList from './post/getTagPostList';
 
 function* RootSaga() {
     yield all([
         getAllTags(),
         takeEvery(actionType.LOAD_POST_LIST, getPostList),
-        takeEvery(actionType.LOAD_POST_CONTENTS, getPostContents)
+        takeEvery(actionType.LOAD_POST_CONTENTS, getPostContents),
+        takeEvery(actionType.LOAD_TAG_POST_LIST, getTagPostList),
     ]);
 };
 
