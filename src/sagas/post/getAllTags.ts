7c683628f42f568857ddd798872fc2cb0a865af3
Isa(yunjongryu) <yunjonglyu@gmail.com>
2021-02-14 17:26:12 +0900

    refactor : SAGAS - GET ALL TAGS DATA STRUCTURE

--- src/sagas/post/getAllTags.ts
+++ src/sagas/post/getAllTags.ts
@@ -6,9 +6,9 @@ function* getAllTags () {
     try {
         const { data } = yield call(api.getAllTags);
         const state : any = {};
-        state.essay = [data.essay];
-        state.tech = [data.tech];
-        state.portfolio = [data.portfolio];
+        state.essay = data.essay;
+        state.tech = data.tech;
+        state.portfolio = data.portfolio;
         yield put(actions.TAGLIST(state));
     } catch(error) {
 
