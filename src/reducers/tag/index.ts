83e2a8b595de518d006bb7548efbe6c0297ecadf
Isa(yunjongryu) <yunjonglyu@gmail.com>
2021-02-14 17:25:20 +0900

    refactor: REDUX REDUCERS - TAGS INIT DATA & TYPE

--- src/reducers/tag/index.ts
+++ src/reducers/tag/index.ts
@@ -3,30 +3,40 @@ import * as actions from '../../actions';
 import * as types from '../../actions/actionTypes';
 
 interface TagListState {
-    tagList : {
-        essay : Array<any>
-        tech : Array<any>
-        portfolio : Array<any>
+    tagList: {
+        essay: Array<TagProps>
+        tech: Array<TagProps>
+        portfolio: Array<TagProps>
     }
 }
-/** 여기서 생각할 점은 각 태그 리스트가 에세이, 기술, 포트폴리오로 나뉜다는 점이다. 어떻게 처리할까? */
-const tempData = [// 해당 데이터를 어떻게 만드냐에 따라 뷰가 달라진다. 뷰모델 부분에서 가공할 필요가 있다.
-    [{ name: 'HTML' }, { name: 'CSS' }, { name: 'JavaScript' }, { name: 'TypeScript' }],
-    [{ name: 'Node.js' }, { name: 'Webpack' }, { name: 'React' }],
+interface TagProps {
+    idx: number
+    name: string
+}
+
+const tempData = [
+    { idx: 1, name: 'HTML' },
+    { idx: 2, name: 'CSS' },
+    { idx: 3, name: 'JavaScript' },
+    { idx: 4, name: 'TypeScript' },
+    { idx: 5, name: 'Node.js' },
+    { idx: 6, name: 'Webpack' },
+    { idx: 7, name: 'React' }
 ];
-const initialState : TagListState = {
-    tagList : {
-        essay : tempData,
-        tech : tempData,
-        portfolio : tempData
+
+const initialState: TagListState = {
+    tagList: {
+        essay: tempData,
+        tech: tempData,
+        portfolio: tempData
     }
 };
 
-const TagList : Reducer<TagListState, actions.TagActions> = (state = initialState, action) => {
-    if(action.type === types.TAG_LIST){
+const TagList: Reducer<TagListState, actions.TagActions> = (state = initialState, action) => {
+    if (action.type === types.TAG_LIST) {
         return {
             ...state,
-            tagList : action.payload
+            tagList: action.payload
         };
     } else {
         return state;
