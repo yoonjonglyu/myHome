54be85a45a7d6e74cdbf4f37cfdcc267f1cff96d
Isa(yunjongryu) <yunjonglyu@gmail.com>
2021-03-09 21:49:07 +0900

    feat(REDUCERS): ROOT REDUCERS 포트폴리오 리듀서 추가

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -4,9 +4,11 @@ import Taps from './tap';
 import TagList from './tag';
 import PostList from './post';
 import BlogPost from './blogPost';
+import PortfolioList from './portfolio/portfolioList';
+import Portfolio from './portfolio/portfolio';
 
 const RootReducers = combineReducers({
-    Taps, TagList, PostList, BlogPost
+    Taps, TagList, PostList, BlogPost, PortfolioList, Portfolio
 });
 
 export default RootReducers;
