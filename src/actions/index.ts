d4166c5871c251dcd962163171b8595a1da7a4fd
Isa(yunjongryu) <yunjonglyu@gmail.com>
2021-03-09 21:29:27 +0900

    feat(REDUX): ROOT ACTIONS 포트폴리오 액션 연결

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -8,3 +8,7 @@ export {POSTLIST, PostActions} from './postList';
 export {LOADPOSTCONTENTS, LoadBlogPostActions} from './loadPostContents';
 export {POSTCONTENTS, BlogPostActions} from './postContents';
 export {LOADTAGPOSTLIST, LoadTagPostActions} from './loadTagPostList';
+export {PORTFOLIOLIST, PortfolioListActions} from './portfolioList';
+export {LOADPORTFOLIOLIST, LoadPortfolioListActions} from './loadPortfolioList';
+export {PORTFOLIOCONTENTS, PortfolioActions} from './portfolioContents';
+export {LOADPORTFOLIOCONTENTS, LoadPortfolioActions} from './loadPortfolioContents';
