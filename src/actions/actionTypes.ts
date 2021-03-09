059cfbbbfdb9b5aee2f207881c4f3153e2430680
Isa(yunjongryu) <yunjonglyu@gmail.com>
2021-03-09 21:18:41 +0900

    feat(REDUX): ACTIONS TYPE - PORTFOLIO LIST & CONTENTS

--- src/actions/actionTypes.ts
+++ src/actions/actionTypes.ts
@@ -5,3 +5,7 @@ export const POST_LIST : string = "postList";
 export const LOAD_POST_CONTENTS : string = "loadPostContents";
 export const POST_CONTENTS : string = "postContents";
 export const LOAD_TAG_POST_LIST : string = "loadTagPostList";
+export const PORTFOLIO_LIST : string = "portfolioList";
+export const LOAD_PORTFOLIO_LIST : string = "loadPortfolioList";
+export const PORTFOLIO_CONTENTS : string = "portfolioContents";
+export const LOAD_PORTFOLIO_CONTENTS : string = "loadPortfolioContents";
