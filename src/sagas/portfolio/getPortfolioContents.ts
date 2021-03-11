e8c5bc112f232a72a5d54dbe57f4371c239b61ec
Isa(yunjongryu) <yunjonglyu@gmail.com>
2021-03-11 22:29:24 +0900

    feat(SAGA): 포트폴리오 사가 - 임시 구조

---
+++ src/sagas/portfolio/getPortfolioContents.ts
@@ -0,0 +1,14 @@
+import { put, call} from 'redux-saga/effects';
+import api from '../../api/blogApi';
+import * as actions from '../../actions';
+
+function* getPortfolioContents () {
+    try {
+        const { data } = yield call(api.getAllTags);
+        yield put(actions.PORTFOLIOCONTENTS(data));
+    } catch (error) {
+        console.error("API 서버에서 데이터를 불러오는데 실패했습니다.");
+    }
+}
+
+export default getPortfolioContents;
---
+++ src/sagas/portfolio/getPortfolioList.ts
@@ -0,0 +1,14 @@
+import { put, call} from 'redux-saga/effects';
+import api from '../../api/blogApi';
+import * as actions from '../../actions';
+
+function* getPortfolioList () {
+    try {
+        const { data } = yield call(api.getAllTags);
+        yield put(actions.PORTFOLIOLIST(data));
+    } catch (error) {
+        console.error("API 서버에서 데이터를 불러오는데 실패했습니다.");
+    }
+}
+
+export default getPortfolioList;
