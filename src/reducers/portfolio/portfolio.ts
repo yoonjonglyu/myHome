621eaed087d064952667f2c0ed2d95ac69eaa15d
Isa(yunjongryu) <yunjonglyu@gmail.com>
2021-03-25 22:43:05 +0900

    feat(REDUCERS): 포트폴리오 리듀서 인터페이스 & 초기 값 정의

--- src/reducers/portfolio/portfolio.ts
+++ src/reducers/portfolio/portfolio.ts
@@ -3,9 +3,19 @@ import * as actions from '../../actions';
 import * as types from '../../actions/actionTypes';
 
 interface PortfolioState {
+    idx : number
+    title : string
+    date : string
+    contents : string
+    tags : Array<{ idx: number, name: string }>  
 }
 
 const initialState : PortfolioState = {
+    idx : 0,
+    title : "test",
+    date : "2021-03-25",
+    contents : "어떻게하면 최대한 게으르면서 좋은 결과물을 만들 수 있을까?",
+    tags : [{idx : 0, name: "학습"}]
 };
 
 const Portfolio : Reducer<PortfolioState, actions.PortfolioActions> = (state = initialState, action) => {
