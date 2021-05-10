f13d7de0748109d9efa6098b6f43f86ac4b8e5a7
Isa(yunjongryu) <yunjonglyu@gmail.com>
2021-05-10 22:17:08 +0900

    refactor(STORE): 상태관리 관련 DIR 구조 변경

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
-import store from './configureStore';
+import Store from './store/configureStore';
 
 import App from './app';
 
 
 const render = (Components: React.FC) => {
     ReactDOM.render(
-        <Provider store={store()}>
+        <Provider store={Store}>
             <Components />
         </Provider>,
         document.getElementById('app')
