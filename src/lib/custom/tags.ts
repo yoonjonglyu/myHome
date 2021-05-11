a679b3788fd3a48b582d6cd0ed60c6a2a78ca8bf
Isa(yunjongryu) <yunjonglyu@gmail.com>
2021-05-11 22:25:59 +0900

    feat(CUSTOM): TAG - 커스텀훅 INIT & TAG LIST

---
+++ src/lib/custom/tags.ts
@@ -0,0 +1,20 @@
+import { useSelector, useDispatch } from 'react-redux';
+
+import { RootState } from '../../store/reducers';
+
+import * as Actions from '../../store/actions';
+
+function tagList() {
+    const { tagList } = useSelector((state: RootState) => ({
+        tagList : state.TagList.tagList
+    }));
+
+    const dispatch = useDispatch();
+    const getTagList = () => {
+        dispatch(Actions.LOADTAGPOSTLIST);
+    }
+    
+    return { tagList, getTagList };
+}
+
+export { tagList }
