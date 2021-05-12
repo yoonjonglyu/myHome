777e5d0d30e734ff939fa6b934cacf94e7f58b52
Isa(yunjongryu) <yunjonglyu@gmail.com>
2021-05-12 23:20:17 +0900

    style: 공백조정

--- src/lib/api/blogApi.ts
+++ src/lib/api/blogApi.ts
@@ -1,28 +1,28 @@
 import axios from 'axios';
 
 class BlogApi {
-    _REST_API_URL : string;
+    _REST_API_URL: string;
 
     constructor() {
         this._REST_API_URL = "https://isa-myblog.herokuapp.com/post/";
     };
 
-    getAllTags =  () => {
+    getAllTags = () => {
         return axios.get(
             this._REST_API_URL + "tags"
         );
     };
-    getPostList = (type : string) => {
+    getPostList = (type: string) => {
         return axios.get(
             `${this._REST_API_URL}${type}`
         );
     };
-    getPostContents = (type : string, idx : string) => {
+    getPostContents = (type: string, idx: string) => {
         return axios.get(
             `${this._REST_API_URL}${type}?idx=${idx}`
         );
     };
-    getTagPostList = (type : string, idx : string) => {
+    getTagPostList = (type: string, idx: string) => {
         return axios.get(
             `${this._REST_API_URL}taglist?type=${type}&idx=${idx}`
         );
