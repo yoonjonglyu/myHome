40792324cc6d233ff9cb5fd3558ea9bb84d0169f
Isa(yunjongryu) <yunjonglyu@gmail.com>
2021-05-12 23:25:57 +0900

    feat(CUSTOM): POST - POST CONTENTS

--- src/lib/custom/post.ts
+++ src/lib/custom/post.ts
@@ -6,16 +6,28 @@ import * as Actions from '../../store/actions';
 
 function postList() {
     const { postList } = useSelector((state: RootState) => ({
-        postList : state.PostList.PostList
+        postList: state.PostList.PostList
     }));
 
     const dispatch = useDispatch();
     const getPostList = () => {
         dispatch(Actions.LOADPOSTLIST);
     }
-    
+
     return { postList, getPostList };
+}
+function postContents() {
+    const { postContents } = useSelector((state: RootState) => ({
+        postContents: state.PostContents.post
+    }));
+
+    const dispatch = useDispatch();
+    const getPostContents = (type: string, idx: string) => {
+        dispatch(Actions.LOADPOSTCONTENTS(type, idx));
+    }
+
+    return { postContents, getPostContents }
 }
 
 
-export { postList }
+export { postList, postContents }
